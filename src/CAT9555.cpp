cb2cd836bfbfec8228058a82fe167d8eba249e70
andrea-83 <andrea@arduino.org>
2016-03-21 12:23:24 +0100

    upgraded library license

--- src/CAT9555.cpp
+++ src/CAT9555.cpp
@@ -1,5 +1,28 @@
-/******************************************************************************
-Arduino_CAT9555.cpp
+/*
+****************************************************************************
+* Copyright (c) 2015 Arduino srl. All right reserved.
+*
+* File : CAT9555.cpp
+* Date : 2016/03/21
+* Revision : 0.0.1 $
+* Author: andrea[at]arduino[dot]org
+*
+****************************************************************************
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
 ******************************************************************************/
 
 #include <CAT9555.h>
@@ -65,7 +88,7 @@ uint8_t CAT9555::read_8_Register(int reg)
 	Wire.beginTransmission(address);
 	Wire.write(reg);
 	Wire.endTransmission();  
-	Wire.requestFrom(address, (uint8_t)1); 
+	Wire.requestFrom(address, (byte)1); 
 	if(Wire.available()) {
 		_data0 = Wire.read();
 	}
@@ -78,7 +101,7 @@ uint16_t CAT9555::read_16_Register(int reg)
 	Wire.beginTransmission(address);
 	Wire.write(reg);
 	Wire.endTransmission();  
-	Wire.requestFrom(address, (uint8_t)2); 
+	Wire.requestFrom(address, (byte)2); 
 	if(Wire.available()) {
 		_data0 = Wire.read() << 8 ;
 	}
--- src/Lucky.h
+++ src/Lucky.h
@@ -1,5 +1,29 @@
-/******************************************************************************
-Arduino.h
+/*
+****************************************************************************
+* This file is part of Arduino Lucky Shield Library
+*
+* Copyright (c) 2016 Arduino srl. All right reserved.
+*
+* File : Lucky.h
+* Date : 2016/03/21
+* Revision : 0.0.1 $
+* Author: andrea[at]arduino[dot]org
+*
+****************************************************************************
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 ******************************************************************************/
 
@@ -19,8 +43,12 @@ class Lucky
 {
 	public:
 		
-		void init(){ 
+		void begin(){ 
 			Wire.begin();
+			cat9555.begin();
+			ssd1306.begin();
+			mag3110.begin();
+  			bme280.begin();
 		}	
 		MMA8491Q& accelerometer(){
 		 	return mma8491q;
@@ -28,7 +56,7 @@ class Lucky
 		MAG3110& magnetometer(){
 		 	return mag3110;
 		}
-		BME280& bosch(){
+		BME280& environment(){
 		 	return bme280;
 		}
 		CAT9555& gpio(){
