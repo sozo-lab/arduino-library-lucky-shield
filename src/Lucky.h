40059fce6e3ee7f91385a8b837a502d85e420314
Doi Yusuke <o111027y@mail.kyutech.jp>
2018-02-23 21:37:54 +0900

    Adjust to sozo-lab production

--- src/Lucky.h
+++ src/Lucky.h
@@ -1,5 +1,34 @@
 /*
+******************************************************************************
+* This file is part of Arduino Lucky Shield Light Library
+*
+* File : Lucky.h
+* Date : 2018/02/23
+* Revision : 1.0.4
+* Author : doi[at]sozolab[dot]jp
+* Original author : andrea[at]arduino[dot]org
+*
 ****************************************************************************
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+******************************************************************************/
+
+
+/*
+*****************************************************************************
 * This file is part of Arduino Lucky Shield Library
 *
 * Copyright (c) 2016 Arduino srl. All right reserved.
@@ -31,11 +60,7 @@
 #define Lucky_h
 
 #include "lib/CAT9555.h"
-#include "lib/MMA8491Q.h"
-#include "lib/MAG3110.h"
 #include "lib/BME280.h"
-#include "lib/Adafruit_GFX.h"
-#include "lib/Adafruit_SSD1306.h"
 #include <Arduino.h>
 #include "Wire.h"
 
@@ -50,24 +75,13 @@ class Lucky
 		void begin(){ 
 			Wire.begin();
 			cat9555.begin();
-			ssd1306.begin();
-			mag3110.begin();
   			bme280.begin();
 		}	
-		MMA8491Q& accelerometer(){
-		 	return mma8491q;
-		}
-		MAG3110& magnetometer(){
-		 	return mag3110;
-		}
 		BME280& environment(){
 		 	return bme280;
 		}
 		CAT9555& gpio(){
 		 	return cat9555;
-		}
-		Adafruit_SSD1306& oled(){
-		 	return ssd1306;
 		}
 };
 
